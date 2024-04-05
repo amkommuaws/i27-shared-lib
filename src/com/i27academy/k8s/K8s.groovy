a6ce4826b81c156c62abdbbe5c0e4a4afd9f5cc3
amar <amarkommu@gmail.com>
2024-04-05 10:14:57 +0530

    added dynamic method

--- src/com/i27academy/k8s/K8s.groovy
+++ src/com/i27academy/k8s/K8s.groovy
@@ -6,14 +6,14 @@ class K8s {
         this.jenkins = jenkins
     }
     
-    def auth_login() {
+    def auth_login(gke_cluster_name, gke_zone, gke_project) {
         jenkins.sh"""#!/bin/bash
         echo "Entering authentication method for GKE cluster login"
         gcloud config set account jenkins@i27projects.iam.gserviceaccount.com
         # gcloud auth activate-service-account jenkins@i27projects.iam.gserviceaccount.com --key-file=key.json
         gcloud compute instances list
         echo "**************Listing number of Nodes in K8s******************"
-        gcloud container clusters get-credentials cart-cluster --zone us-west1-a --project i27projects
+        gcloud container clusters get-credentials $gke_cluster_name --zone $gke_zone --project $gke_project
         kubectl get nodes
         """
     }
