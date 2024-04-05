50eb70084ec6678f5d7c3d6af042ef068747125b
amar <amarkommu@gmail.com>
2024-04-05 07:31:57 +0530

    added first code

---
+++ src/com/i27academy/builds/Calculator.groovy
@@ -0,0 +1,11 @@
+package com.i27academy.builds
+
+class Calculator{
+    def jenkins
+    
+    Calculator(jenkins) {
+        this.jenkins = jenkins
+    }
+
+
+}
---
+++ src/com/i27academy/builds/Docker.groovy
@@ -0,0 +1,22 @@
+package com.i27academy.builds
+
+class Docker {
+    def jenkins
+    Docker(jenkins) {
+        this.jenkins = jenkins
+    }
+    //Addition Method
+    def add(firstNumber, secondNumber) {
+        //logic
+        return firstNumber+secondNumber 
+    }
+
+
+    //Application Build method
+    def buildApp() {
+        jenkins.sh"""#!/bin/bash
+        echo "Building the Eureka Application"
+        mvn clean package -DskipTests=true
+        """
+    }
+}
